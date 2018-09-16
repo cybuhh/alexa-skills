84af16c4957c20bd82f77560d231e550a77b2c34
cybuhh <>
2018-09-16 14:48:38 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+https://developer.amazon.com/alexa/console/ask/test/amzn1.ask.skill.68aa70a0-9526-4a89-a994-2e2521a93313/development/en_US/
+
+https://developer.amazon.com/alexa/console/ask/build/custom/amzn1.ask.skill.68aa70a0-9526-4a89-a994-2e2521a93313/development/en_US/intents/turn
---
+++ index.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const alexa = require('alexa-app');
+
+const port = process.env.port || 3000
+const app = express();
+ 
+const alexaApp = new alexa.app('tv');
+
+// setup the alexa app and attach it to express before anything else
+alexaApp.express({
+    expressApp: app,
+    checkCert: false,
+    debug: true
+});
+
+// ask my remote to turn on
+alexaApp.intent('turn', {
+    'slots': { 'status': 'status' },
+    'utterances': ['turn {status}']
+  },
+  function(request, response) {
+    const status = request.slot('status');
+    console.log('status', status);
+    response.say(`You asked to change status to ${status}`);
+  }
+);
+
+alexaApp.intent("AMAZON.HelpIntent", {
+    "slots": {},
+    "utterances": []
+  },
+  function(request, response) {
+    var helpOutput = "You can say 'ask my remote to turn on'. You can also say stop or exit to quit.";
+    var reprompt = "What would you like to do?";
+    // AMAZON.HelpIntent must leave session open -> .shouldEndSession(false)
+    response.say(helpOutput).reprompt(reprompt).shouldEndSession(false);
+  }
+);
+ 
+alexaApp.intent("AMAZON.StopIntent", {
+    "slots": {},
+    "utterances": []
+  }, function(request, response) {
+    var stopOutput = "Don't You Worry. I'll be back.";
+    response.say(stopOutput);
+  }
+);
+ 
+alexaApp.intent("AMAZON.CancelIntent", {
+    "slots": {},
+    "utterances": []
+  }, function(request, response) {
+    var cancelOutput = "No problem. Request cancelled.";
+    response.say(cancelOutput);
+  }
+);
+
+alexaApp.launch(function(request, response) {
+    response.say("You launched the app!");
+});
+  
+// now POST calls to /sample in express will be handled by the app.request() function
+// GET calls will not be handled
+
+app.get('/', function (req, res) {
+    res.send('Hello World')
+})
+
+// from here on, you can setup any other express routes or middleware as normal
+app.listen(port);
+console.log(`Listening on port ${port} try http://localhost: ${port}`);
